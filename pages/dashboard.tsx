4f4b6c75dd0202bfa864c8911eda1639c0f87038
Siddharth Srinivasan <sidsrinivasan6@gmail.com>
2023-04-08 13:37:01 -0700

    cooked up some framer motion and tailwind

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
-const Home: NextPage = () => {
+const Dash: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
@@ -12,4 +12,4 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export default Dash
